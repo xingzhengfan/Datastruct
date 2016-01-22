415449b72071bb1d71a95b28b9efaf195d655d70
huangziliang <huangzl@putao.com>
2016-01-22 17:25:46 +0800

    Initial Commit

---
+++ Datastruct/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Datastruct
+//
+//  Created by so on 16/1/22.
+//  Copyright © 2016年 PT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
